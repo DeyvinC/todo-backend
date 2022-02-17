b4e5f2b67685e0f07deffcff4d10f52f85804c04
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-02-17 10:57:14 -0500

    framed api

---
+++ src/tasks.js
@@ -0,0 +1,14 @@
+// exports.createTask = (req, res) => {
+
+// const { response } = require("express")
+
+    
+// }
+
+exports.getTasks = (req, res) => {
+    res.send('Get tasks is working.');
+}
+
+// exports.updateTasks = (req, res) => {
+
+// }
